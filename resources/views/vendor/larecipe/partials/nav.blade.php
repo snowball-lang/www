7b72790bedce88d580961f261d76d79405e6ab97
mauro-balades <mauro.balades@tutanota.com>
2022-10-05 17:15:26 +0200

    changed theme colors for documentation

--- resources/views/vendor/larecipe/partials/nav.blade.php
+++ resources/views/vendor/larecipe/partials/nav.blade.php
@@ -1,71 +1,118 @@
-<div class="fixed pin-t pin-x z-40">
-    <div class="bg-gradient-primary text-white h-1"></div>
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <nav class="flex items-center justify-between text-black bg-navbar shadow-xs h-16">
-        <div class="flex items-center flex-no-shrink">
-            <a href="{{ url('/') }}" class="flex items-center flex-no-shrink text-black mx-4">
-                @include("larecipe::partials.logo")
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('welcome') }}">
+                        <x-application-logo class="block h-16 w-auto fill-current text-gray-600" />
+                    </a>
+                </div>
 
-                <p class="inline-block font-semibold mx-1 text-grey-dark">
-                    {{ config('app.name') }}
-                </p>
-            </a>
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="/docs" :docs="true" :active="true">
+                        {{ __('Documentation') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('pkgs')" :active="request()->routeIs('pkgs')">
+                        {{ __('Packages') }}
+                    </x-nav-link>
+                </div>
+            </div>
 
-            <div class="switch">
-                <input type="checkbox" name="1" id="1" v-model="sidebar" class="switch-checkbox" />
-                <label class="switch-label" for="1"></label>
+            <!-- Settings Dropdown -->
+            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                @auth
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <div>{{ Auth::user()->name }}</div>
+
+                            <div class="ml-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 font-bold">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 font-bold">Register</a>
+                    @endif
+                @endauth
+            </div>
+
+            <!-- Hamburger -->
+            <div class="-mr-2 flex items-center sm:hidden">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
             </div>
         </div>
+    </div>
 
-        <div class="block mx-4 flex items-center">
-            @if(config('larecipe.search.enabled'))
-                <larecipe-button id="search-button"
-                    :type="searchBox ? 'primary' : 'link'"
-                    @click="searchBox = ! searchBox"
-                    class="px-4">
-                    <i class="fas fa-search" id="search-button-icon"></i>
-                </larecipe-button>
+    <!-- Responsive Navigation Menu -->
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        @auth
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link href="/docs">
+                    {{ __('Documentation') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('pkgs')" :active="request()->routeIs('pkgs')">
+                    {{ __('Packages') }}
+                </x-responsive-nav-link>
+            </div>
+
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="px-4">
+                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                </div>
+
+                <div class="mt-3 space-y-1">
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
+            </div>
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+    
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
             @endif
-
-            <larecipe-button tag="a" href="https://github.com/saleem-hadad/larecipe" target="__blank" type="black" class="mx-2 px-4">
-                <i class="fab fa-github"></i>
-            </larecipe-button>
-
-            {{-- versions dropdown --}}
-            <larecipe-dropdown>
-                <larecipe-button type="primary" class="flex">
-                    {{ $currentVersion }} <i class="mx-1 fa fa-angle-down"></i>
-                </larecipe-button>
-
-                <template slot="list">
-                    <ul class="list-reset">
-                        @foreach ($versions as $version)
-                            <li class="py-2 hover:bg-grey-lightest">
-                                <a class="px-6 text-grey-darkest" href="{{ route('larecipe.show', ['version' => $version, 'page' => $currentSection]) }}">{{ $version }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </template>
-            </larecipe-dropdown>
-            {{-- /versions dropdown --}}
-
-            @auth
-                {{-- account --}}
-                <larecipe-dropdown>
-                    <larecipe-button type="white" class="ml-2">
-                        {{ auth()->user()->name ?? 'Account' }} <i class="fa fa-angle-down"></i>
-                    </larecipe-button>
-
-                    <template slot="list">
-                        <form action="/logout" method="POST">
-                            {{ csrf_field() }}
-
-                            <button type="submit" class="py-2 px-4 text-white bg-danger inline-flex"><i class="fa fa-power-off mr-2"></i> Logout</button>
-                        </form>
-                    </template>
-                </larecipe-dropdown>
-                {{-- /account --}}
-            @endauth
-        </div>
-    </nav>
-</div>
+        @endauth
+    </div>
+</nav>
