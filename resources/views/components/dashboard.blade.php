89e72b3b8f20c51c8568fb2f5096085a0e2739a5
mauro-balades <mauro.balades@tutanota.com>
2022-10-13 22:51:25 +0200

    added a top nav

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -55,7 +55,7 @@
                         <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
 
-                    <a href="https://laravel.bigcartel.com" class="ml-1 underline">
+                    <a href="https://laravel.bigcartel.com" class="ml-1">
                         Shop
                     </a>
 
@@ -63,14 +63,14 @@
                         <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                     </svg>
 
-                    <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
+                    <a href="https://github.com/sponsors/taylorotwell" class="ml-1">
                         Sponsor
                     </a>
                 </div>
             </div>
 
             <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                Snowball v1.0.0
             </div>
         </div>
     </div>
